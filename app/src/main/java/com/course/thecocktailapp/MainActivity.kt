b31c84f5de33867eb5e7f68bddb1cc8a972b5f7d
Emircan Duyal <emircanduyal@gmail.com>
2022-09-12 15:13:41 +0300

    MainActivity created with dummy items

--- app/src/main/java/com/course/thecocktailapp/MainActivity.kt
+++ app/src/main/java/com/course/thecocktailapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.course.thecocktailapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+private lateinit var listAdapter: CocktailListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager (this)
+
+        val adapter =
+            CocktailListAdapter(DummyList.getDummyCocktailItems() as ArrayList<CocktailModel>)
+
+        recyclerView.adapter = adapter
     }
 }
